25db2d4f9d640767d180d7733d22eedad52c429f
Kyotun <emirpisirici@hotmail.com>
2023-11-17 22:21:42 +0100

    Creating a lucky combination

--- one/basics.py
+++ one/basics.py
@@ -1,1 +1,5 @@
-print("Nice to meet you " + input("Your name: ") + "!")
+print("Hello User.")
+colour = input("What is your favourite colour?\n")
+number = input("What about lucky number?\n")
+print("Your lucky combination is: " + str(len(colour)) + number)
+
