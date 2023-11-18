6a0eac2629e77e90dcb77ec6aabc86bf40dd5a75
Kyotun <emirpisirici@hotmail.com>
2023-11-18 08:44:39 +0100

    Bill calculator

--- one/basics.py
+++ one/basics.py
@@ -1,21 +1,15 @@
-#Week calculator
-#In 1 year there is total of 52 weeks
-#Max year is 90
-one_year_in_weeks = 52
-max_age = 90
+#Tip calculator
 
-print("Hey User.")
-age = input("Please enter your age: ")
-age_as_int = int(age)
+print("Hey user. Welcome to the tip calculator")
 
-#Calculate the actual age in weeks
-age_in_weeks = age_as_int * one_year_in_weeks
+total_bill = input("Please enter the total amount of the bill: ")
+percentage_to_tip = input("Could you please enter the percentage to tip: ")
 
-#Calculate the total weeks have been lived
-life_limit_in_weeks = max_age * one_year_in_weeks
+tip = float(total_bill) / float(percentage_to_tip)
 
-#Calculate the total weeks left
-weeks_left = life_limit_in_weeks - age_in_weeks
+bill_plus_tip = float(total_bill) + tip
 
-print(f"You have {weeks_left} weeks left.")
+number_people_paying = input("Please enter the number of people are paying: ")
 
+each_pay = bill_plus_tip / float(number_people_paying)
+print(f"Each person should pay: {each_pay}")
