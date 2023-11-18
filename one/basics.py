2294775b97e13f1d2c59c9690c97575a18408389
Kyotun <emirpisirici@hotmail.com>
2023-11-18 07:28:51 +0100

    BMI Calculator.

--- one/basics.py
+++ one/basics.py
@@ -1,5 +1,17 @@
-print("Hello User.")
-colour = input("What is your favourite colour?\n")
-number = input("What about lucky number?\n")
-print("Your lucky combination is: " + str(len(colour)) + number)
+#BMI Calculator
+
+print("Hey user.")
+
+#Input function stores inputs as 'str' in variables.
+#For division we need to type cast from 'str' to 'float'.
+height = input("Please enter your height in meter please: ")
+height_float = float(height)
+
+print("Thank you.")
+
+weight = input("Could you enter your weight in kilograms too: ")
+weight_float = float(weight)
+
+#Turn division to str to be able to concat the strings.
+print("Your BMI is: " + str(weight_float/(height_float**2)))
 
