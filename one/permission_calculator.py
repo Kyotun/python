0976c15f358d1fe053ce5c89f6c945d1ee993023
Kyotun <emirpisirici@hotmail.com>
2023-11-19 09:45:42 +0100

    Permission calculator v2.

--- one/permission_calculator.py
+++ one/permission_calculator.py
@@ -6,12 +6,23 @@ height = int(input("Please enter your height: "))
 if height > 120:
     print("You can ride with rollercoaster!\nBut we need to check your age for price too!")
     age = int(input("Please enter your age: "))
+
     if age < 12:
-        print("For under 12 is price $5! Enjoy the ride.")
+        bill = 5
+        print("Child tickets are $5! Enjoy the ride.")
     elif age < 18:
-        print("For the age between 12-18 the price is $7. Enjoy the ride!")
+        bill = 7
+        print("For youths the price is $7. Enjoy the ride!")
     else:
-        print("So you're over 18. Price is gonna be $10. Enjoy the ride!")
+        bill = 10
+        print("So you're adult 18. Price is gonna be $10. Enjoy the ride!")
+    
+    wants_photo = input("By the way, do you want a photo taken? Y or N:")
+    if wants_photo == 'Y':
+        bill += 3
+
+    print(f"The bill is {bill}")
+
 else:
     print("Unfortunately you're not allowed to ride with rollercoaster...")
     print("Please come back when you're higher than 120. Untill then!")
