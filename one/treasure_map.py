55a045af97f8e15225eb5ee2d1fb1aba61902057
Kyotun <emirpisirici@hotmail.com>
2023-11-19 18:32:05 +0100

    Treasure map 3x3

---
+++ one/treasure_map.py
@@ -0,0 +1,22 @@
+#Trasure map game
+
+line1 = ["□","□","□"]
+line2 = ["□","□","□"]
+line3 = ["□","□","□"]
+map = [line1, line2, line3]
+abc= ["a", "b", "c"]
+
+print(f"{line1}\n{line2}\n{line3}")
+print("Hiding your treasure! X marks the spot.")
+position = input("Where do you want to store the treasure?\n")
+
+letter = position[0].lower()
+letter_index = abc.index(letter)
+
+number_index = int(position[1])-1
+
+map[number_index][letter_index] = 'X'
+
+print(f"{line1}\n{line2}\n{line3}")
+
+
