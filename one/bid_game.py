b2e03244db99eb92737b0f7798462880f4b69fe8
Kyotun <emirpisirici@hotmail.com>
2023-12-09 10:45:02 +0100

    Bid game V2.

--- one/bid_game.py
+++ one/bid_game.py
@@ -2,17 +2,24 @@
 #Secret bid game.
 #Every person bids, if the bidder gave the highest amount His/Her name will stay in the winnder list.
 #Winner list is dictionary in our case.
+
 import os
 
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def find_highest_bid(bid_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bid_record:
+        bid_amount = bid_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with the bid of ${highest_bid}")
 
+bids = {}
 answer = ""
-winner_dict = {
-    "name": "empty",
-    "bid" : 0
-}
 
 print("Welcome to the bid game!")
 print("Game starts now.")
@@ -21,13 +28,10 @@ while answer != "no":
 
     name = input("What is your name?: ")
     bid = int(input("what is your bid?: $"))
-
-    if(bid > winner_dict["bid"]):
-        winner_dict["name"] = name
-        winner_dict["bid"] = bid
+    bids[name] = bid
 
     answer = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
 
     clear_console()
 
-print(f"The winner is {winner_dict["name"]} with a bid of ${winner_dict["bid"]}!")
+find_highest_bid(bids)
