bdbc6986f5835d6fadcaeb507d7228317c96e8a4
Kyotun <emirpisirici@hotmail.com>
2023-12-11 13:04:08 +0100

    Reorganized the files.

--- one/even_or_odd_calculator.py
+++
@@ -1,9 +0,0 @@
-#Even or odd calculator
-
-number = int(input("Hey! Please enter a number: "))
-print("Let's see if it is an even or an odd number.")
-
-if(number % 2 == 0):
-    print("The number is even!")
-else:
-    print("The number is odd!")
--- one/lucky_combination_calculator.py
+++
@@ -1,6 +0,0 @@
-#Lucky combination calculator(1)
-
-print("Hello User.")
-colour = input("What is your favourite colour?\n")
-number = input("What about lucky number?\n")
-print("Your lucky combination is: " + str(len(colour)) + number)
