32e297ffb3fb30a9d2247ba302635cf32ccf8a84
Kyotun <emirpisirici@hotmail.com>
2023-12-12 18:34:23 +0100

    OOP basics, prettytable module: create a class object and change its attributes.

--- basics/OOP_basics/oop_example.py
+++ basics/OOP_basics/oop_example.py
@@ -1,6 +1,14 @@
 from prettytable import  PrettyTable
 
+# Create a PrettyTable object.
 table = PrettyTable()
+
+# Add column name and column variables.
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Tychoon"])
 table.add_column("Type", ["Electric", "Water", "Fire"])
+
+# Align the table to the left.
+table.align = "l"
+
+# Print the table.
 print(table)
