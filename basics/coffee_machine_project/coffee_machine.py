3181b2decd6000490751d5f2c2c559c8531f4c73
Kyotun <emirpisirici@hotmail.com>
2023-12-12 11:59:21 +0100

    Coffee Machine Project V2.

--- basics/coffee_machine_project/coffee_machine.py
+++ basics/coffee_machine_project/coffee_machine.py
@@ -5,6 +5,7 @@ def money_calculator(order):
     """Take inserted money and order as input.
         If the inserted money >= cost of the order, print the prepared object and change amount.
         If money is not enough prints it's not enough."""
+    order = format_input(user_input=order)
     total_money_inserted = 0
     cost = MENU[order]["cost"]
     print("Please insert coins")
@@ -42,13 +43,14 @@ def is_resource_sufficient(order):
     """Checks if the resources for chosen order is enough.
         Returns True if it's enough.
         Returns False if it's not enough."""
+    order = format_input(user_input=order)
     is_sufficient = False
     if resources["milk"] < MENU[order]["ingredients"]["milk"]:
-        print("Sorry there is not enough milk.")
+        print(f"Sorry there is not enough milk for {order}.")
     elif resources["water"] < MENU[order]["ingredients"]["water"]:
-        print("Sorry there is not enough water.")
+        print(f"Sorry there is not enough water for {order}.")
     elif resources["coffee"] < MENU[order]["ingredients"]["coffee"]:
-        print("Sorry there is not enough coffee.")
+        print(f"Sorry there is not enough coffee for {order}.")
     else:
         is_sufficient = True
     return is_sufficient
@@ -83,27 +85,51 @@ def refill_coffee():
 
 def check_wish(user_input):
     """Takes user input and calls the function according to that wish.
-        If user wishes to close the machine with 'off' order, returns False"""
-    if user_input == "off":
+        If user wishes to close the machine with integer input 8, returns False.
+        Otherwise, do the wished action and returns True."""
+    if user_input == 8:
         return False
-    elif user_input == "report":
+    elif user_input == 7:
         report()
-    elif user_input == "refill water":
+    elif user_input == 4:
         refill_water()
-    elif user_input == "refill milk":
+    elif user_input == 5:
         refill_milk()
-    elif user_input == "refill coffee":
+    elif user_input == 6:
         refill_coffee()
     elif is_resource_sufficient(order=user_input):
         money_calculator(order=user_input)
     return True
 
 
+def format_input(user_input):
+    """Convert the integer input into requested order.
+        If input is 1 convert it to 'espresso'
+        2 will be 'cappuccino'
+        3 is going to be 'latte'."""
+    if user_input == 1:
+        return "espresso"
+    elif user_input == 2:
+        return "cappuccino"
+    elif user_input == 3:
+        return "latte"
+    else:
+        print("Input is invalid.")
+
+
 state_on = True
 
 while state_on:
-    # TODO: prepare a menu like 1- ..., 2-..., etc.
-    wish = input("What would you like? (espresso/latte/cappuccino): ")
+    print("Hello!")
+    print("1- Make espresso.")
+    print("2- Make cappuccino.")
+    print("3- Make latte.")
+    print("4- Refill water.")
+    print("5- Refill milk.")
+    print("6- Refill coffee.")
+    print("7- Report the resources.")
+    print("8- Close the machine.")
+    wish = int(input("What would you like to do?: "))
     state_on = check_wish(user_input=wish)
 
 print("See you later, have a nice day!")
