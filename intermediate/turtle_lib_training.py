b011985b162892883252c3ef86f0ab65a3996ddc
Kyotun <emirpisirici@hotmail.com>
2023-12-13 18:12:22 +0100

    Drawing a spirograph with turtle.

--- intermediate/turtle_lib_training.py
+++ intermediate/turtle_lib_training.py
@@ -7,21 +7,20 @@ def random_color():
     b = random.randint(0,255)
     return (r, g, b)
 
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        alex.color(random_color())
+        alex.circle(50)
+        alex.setheading(alex.heading() + size_of_gap)
+
 t.colormode(255)
 alex = t.Turtle()
 alex.shape("turtle")
-alex.color("red")
-directions = [0, 90, 180, 270, 360]
-alex_speed_list = ["fastest", "fast", "slow", "normal", "slowest"]
+heading = 5
+alex.speed("fastest")
 
-pen_color = (random.randint(0,255), random.randint(0,255), random.randint(0,255) )
-for _ in range(300):
-    alex.pensize(random.randint(1,15))
-    alex.color(random_color())
-    alex.speed(random.choice(alex_speed_list))
-    alex.forward(20)
-    alex.setheading(random.choice(directions))
-
+draw_spirograph(5)
 
 screen = t.Screen()
 screen.exitonclick()
