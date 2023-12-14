a3af989b6f1666f0b6139f1d0c39b8b82a18450c
Kyotun <emirpisirici@hotmail.com>
2023-12-14 15:37:10 +0100

    Forgot to add the draw_spinograph_turtle.py document. Added it.

---
+++ intermediate/draw_spinograph_turtle.py
@@ -0,0 +1,28 @@
+import turtle as t
+import random
+
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    return (r, g, b)
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        alex.color(random_color())
+        alex.circle(50)
+        alex.setheading(alex.heading() + size_of_gap)
+
+
+t.colormode(255)
+alex = t.Turtle()
+alex.shape("turtle")
+heading = 5
+alex.speed("fastest")
+
+draw_spirograph(5)
+
+screen = t.Screen()
+screen.exitonclick()
