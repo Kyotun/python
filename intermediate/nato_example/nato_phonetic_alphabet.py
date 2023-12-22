f83d720968fbb3e2452a2cf223821d2ceae9d1b4
Kyotun <emirpisirici@hotmail.com>
2023-12-22 22:45:10 +0100

    Nato phonetic example is edited: try-except block has been added. Catches the inputs other than alphabet letters.

--- intermediate/nato_example/nato_phonetic_alphabet.py
+++ intermediate/nato_example/nato_phonetic_alphabet.py
@@ -2,12 +2,19 @@ import pandas as pd
 
 # An Example to read someones name and check which characters are matching with the phonetic alphabet of NATO.
 # Add the matching characters codes to the list
-phonetic_dict = pd.read_csv("intermediate/nato_example/nato_phonetic_alphabet.csv")
-phonetic_code_list = []
+data = pd.read_csv("/Users/kyotun/Desktop/python/intermediate/nato_example/nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 
-name = input("Please enter your name: ").upper()
-for (index, row) in phonetic_dict.iterrows():
-    if row.letter in name:
-        phonetic_code_list.append(row.code)
-    
-print(phonetic_code_list)
+is_on = True
+phonetic_code_list = []
+while is_on:
+    try:
+        name = input("Please enter your name: ").upper()
+        phonetic_code_list = [phonetic_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+    else:
+        print("Thank you for the entry.")
+        print("The phonetic code is like this:")
+        print(phonetic_code_list)
+        is_on = False
