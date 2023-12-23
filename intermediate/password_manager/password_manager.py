7f327865211a0e2857b137356f9ffd0ab436f114
Kyotun <emirpisirici@hotmail.com>
2023-12-23 13:18:59 +0100

    Password Generator app update: New informations will be appended to the existing data. Learned: loading/updating the json formated datas.

--- intermediate/password_manager/password_manager.py
+++ intermediate/password_manager/password_manager.py
@@ -30,14 +30,24 @@ def save_password():
                             \nPassword: {password} 
                             \nWebsite: {website}""")
         if answer:
-            with open("intermediate/password_manager/data.json", mode="w") as file:
-                json.dump(new_data_to_dump, file)
-                website_entry.delete(0,END)
-                email_username_entry.delete(0,END)
-                password_entry.delete(0,END)
-                website_entry.focus()
-                messagebox.showinfo(title="Confirmation", message="Informations are saved!")
+            with open("/Users/kyotun/Desktop/python/intermediate/password_manager/data.json", mode="r") as file:
+                # Load the existed json data.
+                loaded_json_data = json.load(file) # Loading a json file. (It will be loaded as dict)
+                
+                # Update the json data with the given inputs from the user.
+                loaded_json_data.update(new_data_to_dump)
                 file.close()
+                
+            with open("/Users/kyotun/Desktop/python/intermediate/password_manager/data.json", mode="w") as file:
+                # Dump the updated data to the same/different json data.
+                json.dump(loaded_json_data, file, indent=4)
+                file.close()
+                
+            website_entry.delete(0,END)
+            email_username_entry.delete(0,END)
+            password_entry.delete(0,END)
+            website_entry.focus()
+            messagebox.showinfo(title="Confirmation", message="Informations are saved!")
         else:
             messagebox.showinfo(title="Acknowledgement", message="Informations are not saved.")
 # ---------------------------- UI SETUP ------------------------------- #
@@ -47,7 +57,7 @@ window.title("Password Manager")
 window.config(padx=20, pady=20, bg="white")
 
 canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
-logo_img = PhotoImage(file="intermediate/password_manager/logo.png")
+logo_img = PhotoImage(file="/Users/kyotun/Desktop/python/intermediate/password_manager/logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
