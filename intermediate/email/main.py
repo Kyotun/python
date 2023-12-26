b90a59096cc8adeff7b5d413a6b875578031b86b
Kyotun <emirpisirici@hotmail.com>
2023-12-26 16:03:39 +0100

    Email sending with 'with' keyword to avoiding noneclosed connection conflicts.

--- intermediate/email/main.py
+++ intermediate/email/main.py
@@ -4,11 +4,11 @@ my_email = "kyottest@gmail.com"
 password = "jkbujogthwkazofw"
 other_email = "emirpisirici@hotmail.com"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, 
-                    to_addrs=other_email, 
-                    msg="Subject:Important.\n\nTesting the body of the email."
-                    )
-connection.close()
+# Create connection with 'with' keyword. (Advantage: connection will be closed at the end by itself.)
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, 
+                        to_addrs=other_email, 
+                        msg="Subject:Important.\n\nTesting the body of the email."
+                        )
