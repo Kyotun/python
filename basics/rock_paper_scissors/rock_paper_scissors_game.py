3e31f3373671ca9e0e48b3188d5d8962af2cbeb6
Kyotun <emirpisirici@hotmail.com>
2023-12-27 16:23:27 +0100

    Rock Paper Scissors game update: Made the code more clear, added explanations.

--- basics/rock_paper_scissors/rock_paper_scissors_game.py
+++ basics/rock_paper_scissors/rock_paper_scissors_game.py
@@ -8,14 +8,27 @@ print("Hey! Welcome the the rock-paper-scissors game!")
 print("Please type 0 for rock, 1 for paper and 2 for scissors.")
 choice = int(input("Please choose one: "))
 
+# 0 -> Rock
+# 1 -> Paper
+# 2 -> Scissors
 if choice > 2 or choice < 0:
+    """
+    Choice should be either 0, 1 or 2.
+    """
     print("Please enter a valid number.")
 else:
     print(f"Your choice: {choice}")
     
+    # Choose a random option for computer.
     choice_random = random.randint(0,2)
     print(f"Computers choice: {choice_random}")
 
+    # 0 -> Rock
+    # 1 -> Paper
+    # 2 -> Scissors
+    # (Substract) 2-1 = 1, Scissors vs Paper (Scissors win)
+    # 1 - 0 = 1, Paper vs Rock (Paper wins)
+    # 0 - 2 = -2, Rock vs Scissors (Rock wins)
     if choice > 2 or choice < 0:
         print("Please enter a valid number.")
     elif choice == choice_random:
