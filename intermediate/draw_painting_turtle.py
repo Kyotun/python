37a16eb586453b53ed4054dec3752933c41c51d4
Kyotun <emirpisirici@hotmail.com>
2023-12-27 16:36:30 +0100

    Drawing a saved painting with turtle update: Made the code more clear, added explanations.

--- intermediate/draw_painting_turtle.py
+++ intermediate/draw_painting_turtle.py
@@ -3,8 +3,11 @@ import random
 import colorgram
 
 rgb_colors = []
+# Colorgram extracts 30 colors from image
 colors = colorgram.extract('/Users/kyotun/Desktop/python_training/intermediate/image.jpeg', 30)
+
 for color in colors:
+    # Extract every r, g and b segment of extracted colors(amount of 30 in this case) of image 'image.jpeg'
     r = color.rgb.r
     g = color.rgb.g
     b = color.rgb.b
@@ -12,13 +15,25 @@ for color in colors:
 
 dot_count = 100
 
+# Create our little turtle, penup cause we don't want to draw lines between the dots.
 t.colormode(255)
 alex = t.Turtle()
 alex.shape("turtle")
 alex.speed("fastest")
 alex.penup()
 
+
 def draw_painting(width, height, step, dot_diameter):
+    """
+    Takes the width and height of frame as parameter.
+    Takes distance between dots as parameter 'step'.
+    Takes dot_diameter as last parameter.
+    In the beginning, moves the turtle a little bit to south west and sets heading to east.
+    Choose a random color from extracted colors from image and put a dot to current location and
+    moves the turtle 1 step(given as parameter) forward.
+    If turtle is at the edge of frame, add 1 to the height and goes beginning of the frame.
+    Continues with putting dots.
+    """
     alex.setheading(225)
     alex.forward(300)
     alex.setheading(0)
@@ -35,9 +50,6 @@ def draw_painting(width, height, step, dot_diameter):
             alex.left(180)
 
 draw_painting(5, 7, 50, 20)
-
-
-
 
 
 screen = t.Screen()
