38480a471c7123a1877022575cb96b8bd0f7c315
Kyotun <emirpisirici@hotmail.com>
2024-01-07 19:20:06 +0100

    Workout/Exercise Tracker update: Added WorkoutTracker class. Testing the functions. Comments are not added yet.

--- intermediate+/workout_tracker/main.py
+++ intermediate+/workout_tracker/main.py
@@ -1,27 +1,51 @@
 import requests
 import os
+import datetime as dt
+from workout_tracker import WorkoutTracker
 
 NUTRI_API_ID = os.environ.get("APP_ID_NUTRI")
 NUTRI_API_KEY = os.environ.get("APP_KEY_NUTRI")
-GENDER = input("Please enter your gender:")
-WEIGHT_KG = input("Please enter your weight in kg:")
-HEIGHT_CM = input("Please enter your height in cm:")
-AGE = input("Please enter your age:")
+GENDER = "male"
+WEIGHT_KG = 80
+HEIGHT_CM = 180
+AGE = 22
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-exercise = input("Which exercise(s) did you do?:")
+sheet_endpoint = "https://api.sheety.co/04fb3676394a9afdc57d00b4868f30df/workouts/tabellenblatt1"
+
+exercise_text = input("Tell me which exercises you did: ")
+
 headers = {
-        "x-app-id": NUTRI_API_ID,
-        "x-app-key": NUTRI_API_KEY
-    }
+    "content-type": "application/json",
+    "x-app-id": NUTRI_API_ID,
+    "x-app-key": NUTRI_API_KEY,
+}
+
 parameters = {
-    "query": exercise,
+    "query": exercise_text,
     "gender": GENDER,
     "weight_kg": WEIGHT_KG,
     "height_cm": HEIGHT_CM,
     "age": AGE
 }
 
-response = requests.post(url=exercise_endpoint, json=parameters, headers=headers)
-json_data = response.json()
-print(json_data)
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+
+################### Start of Step 4 Solution ######################
+
+date_today = dt.datetime.now().strftime("%d/%m/%Y")
+time_now = dt.datetime.now().strftime("%X")
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "tabellenblatt1": {
+            "date": date_today,
+            "time": time_now,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    sheet_response = requests.post(url=sheet_endpoint, json=sheet_inputs)
+    print(sheet_response)
+    print(sheet_response.text)
--- intermediate+/workout_tracker/workout_tracker.py
+++ intermediate+/workout_tracker/workout_tracker.py
@@ -1,9 +1,76 @@
 import requests
 import os
+import datetime as dt
+from requests.auth import HTTPBasicAuth
 
-class workoutTracker():
+class WorkoutTracker():
     def __init__(self) -> None:
-        self.gender = ""
-        self.height = ""
-        self.weight = ""
-        self.age = ""
+        self.gender:str = ""
+        self.height:int = 0
+        self.weight:int = 0
+        self.age:int = 0
+        self.name:str = ""
+        
+        self.email:str = ""
+        self.headers:str= {"x-app-id": "", "x-app-key": "", "content-type": "application/json"}
+        self.exercise_endpoint:str = "https://trackapi.nutritionix.com/v2/natural/exercise"
+        self.sheet_endpoint:str = "https://api.sheety.co/04fb3676394a9afdc57d00b4868f30df/workouts/tabellenblatt1"
+        
+        self.physical_properties()
+        self.account_infos()
+        
+        
+    def physical_properties(self) -> None:
+        self.name = input("Please enter your name(eg. Alex Mustermann): ")
+        self.gender = input("Please enter your gender: ")
+        self.weight = input("Please enter your weight in kg: ")
+        self.height = input("Please enter your height in cm: ")
+        self.age = input("Please enter your age: ")
+    
+    
+    def account_infos(self) -> None:
+        self.email = input("Please enter your email: ")
+        self.headers["x-app-id"] = input("Please enter your Nutritionix APP ID: ")
+        self.headers["x-app-key"] = input("Please enter your Nutritionix APP Key: ")
+    
+    
+    def add_exercise(self) -> None:
+        date_today = dt.datetime.now().strftime("%d/%m/%Y")
+        time_now = dt.datetime.now().strftime("%X")
+        # self.sheet_endpoint = input("Please give the URL of the sheet endpoint from sheety.")
+        # sheet_name = input("Please enter the sheetname:")
+        exercise = input("Which exercise(s) did you do?:")
+        json_data = self.get_exercise_properties(exercise=exercise)
+
+        for exercise in json_data["exercises"]:
+            sheet_inputs = {
+                "tabellenblatt1": {
+                    "date": date_today,
+                    "time": time_now,
+                    "exercise": exercise["name"].title(),
+                    "duration": exercise["duration_min"],
+                    "calories": exercise["nf_calories"]
+                }
+            }
+            sheet_response = requests.post(url=self.sheet_endpoint, json=sheet_inputs)
+            
+            
+    def get_exercise_properties(self, exercise:str) -> dict:
+        parameters = {
+            "query": exercise,
+            "age": self.age,
+            "gender": self.gender,
+            "weight_kg": self.weight,
+            "height_cm": self.height
+        }
+        response = requests.post(url=self.exercise_endpoint, json=parameters, headers=self.headers)
+        json_data = response.json()
+        return json_data
+        
+        
+    def set_sheet_url(self, url:str) -> None:
+        self.sheet_url = url
+    
+    
+    def set_exercise_endpoint(self, url:str) -> None:
+        self.exercise_endpoint = url
