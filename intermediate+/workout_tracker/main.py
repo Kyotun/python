fde8c8243f7d6f10a9ff4210f055011909d831a7
Kyotun <emirpisirici@hotmail.com>
2024-01-09 18:09:16 +0100

    Workout/Exercise Tracker update: Added functions(setters, getters, checkers). Main menu is expanded. More comments should be added not finished with the comments. Looks readable but can be better.

--- intermediate+/workout_tracker/main.py
+++ intermediate+/workout_tracker/main.py
@@ -1,19 +1,44 @@
 from workout_tracker import WorkoutTracker
 from tracker_exception import TrackerException
 
+def account_infos():
+    app_id = input("Please enter your APP Id from Sheety website.")
+    app_key = input("Please enter your APP Id from Sheety website.")
+    my_tracker.set_account_infos(app_id=app_id, app_key=app_key)
+    
+    
+def sheet_infos():
+    authorization = ""
+    if (input("Do your sheet has an authorization token(basic or bearer)? 'y' or 'n': ").lower()) == 'y':
+        authorization = input("Please enter your authorization token: ")
+    sheet_name = input("Please enter the sheet name: ")
+    my_tracker.set_sheet_infos(sheet_name=sheet_name, authorization=authorization)
+    
+    
+def physical_infos():
+    gender = input("Please enter your gender('male' or 'female'): ").lower()
+    weight = int(input("Please enter your weight(in kg, just numbers): "))
+    height = int(input("Please enter your height(in cm, just numbers): "))
+    age = int(input("Please enter your age(in year(s), just numbers): "))
+    my_tracker.set_personal_properties(gender=gender, age=age, height=height, weight=weight)
+    
+    
 print("Welcome to the Workout Tracker Prgoram!")
 print("Firstly, your informations are needed for executing.")
+my_tracker = WorkoutTracker()
 
 is_on = True
 while is_on:
     try:
-        my_tracker = WorkoutTracker()
+        account_infos()
+        sheet_infos()
+        physical_infos()
     except ValueError as e:
         print("Error: ", e)
         print("Please be careful by giving the values. Some values should be number rather then character.")
         print("For example -> Age, height, weight, etc.")
     except TrackerException as e:
-        print("Error by setting up the personal properties: ", e)
+        print("Error: ", e)
         print("Please try again.")
     else:
         is_on = False
@@ -23,40 +48,51 @@ while is_on:
     print("Options:")
     print("1)Add exercise to sheet.")
     print("2)Show entries of workouts.")
-    print("3)Change the sheet url from sheety web.")
+    print("3)Change the sheet endpoint url from sheety web.")
     print("4)Change the existing sheet name.")
-    print("5)Give physical informations again.")
-    print("6)Give accocunt informations again.")
-    print("7)Change the current weight.")
-    print("8)Change the current age.")
-    print("9)Change the current gender.")
-    print("10)Change the current height.")
-    print("11)Exit.")
+    print("5)Change the exercise endpoint url.")
+    print("6)Give physical informations again.")
+    print("7)Give accocunt informations again.")
+    print("8)Give sheet informations again.")
+    print("9)Change the current weight.")
+    print("10)Change the current age.")
+    print("11)Change the current gender.")
+    print("12)Change the current height.")
+    print("13)Exit.")
     try:
-        answer = int(input("Please choose an option:"))
+        answer = int(input("Please choose an option: "))
         if answer == 1:
             my_tracker.add_exercise()
         elif answer == 2:
             my_tracker.get_rows()
         elif answer == 3:
-            sheet_url = input("Please give a valid sheet url: ")
+            sheet_url = input("Please give a valid sheet endpoint url: ")
             my_tracker.set_sheet_url(url=sheet_url)
         elif answer == 4:
             sheet_name = input("Please give a valid sheet name: ")
             my_tracker.set_sheet_name(sheet_name=sheet_name)
         elif answer == 5:
-            my_tracker.set_personal_properties()
+            exercise_url = input("Please give a valid exercise endpoint url: ")
+            my_tracker.set_exercise_url(url=exercise_url)
         elif answer == 6:
-            my_tracker.set_account_infos()
+            physical_infos()
         elif answer == 7:
-            my_tracker.set_weight()
+            account_infos()
         elif answer == 8:
-            my_tracker.set_age()
+            sheet_infos()
         elif answer == 9:
-            my_tracker.set_gender()
+            weight = int(input("Please enter your weight: "))
+            my_tracker.set_weight(weight=weight)
         elif answer == 10:
-            my_tracker.set_height()
+            age = int(input("Please enter your age: "))
+            my_tracker.set_age()
         elif answer == 11:
+            gender = int(input("Please enter your gender(male or female): "))
+            my_tracker.set_gender()
+        elif answer == 12:
+            height = int(input("Please enter your height: "))
+            my_tracker.set_height()
+        elif answer == 13:
             is_on = False
             print("See you later!")
         else:
@@ -65,6 +101,7 @@ while is_on:
         print("Error: ", e)
         print("Please be careful by giving the values. Some values should be number rather then character.")
         print("For example -> Option, age, height, weight, etc.")
+        print("URLs and sheet names should be accurate too.")
     except TrackerException as e:
         print("Error, message: ", e)
         
--- intermediate+/workout_tracker/workout_tracker.py
+++ intermediate+/workout_tracker/workout_tracker.py
@@ -7,38 +7,40 @@ import validators
 APP_ID = os.environ.get("APP_ID_NUTRI")
 APP_KEY = os.environ.get("APP_KEY_NUTRI")
 AUTH_KEY_BEARER = os.environ.get("APP_AUTH_NUTRI")
+EXERCISE_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEET_ENDPOINT = "https://api.sheety.co/04fb3676394a9afdc57d00b4868f30df/workouts/tabellenblatt1"
 
 class WorkoutTracker():
+    """Tracker of workouts.
+    Takes the exercise informations from nutritionix website and saves the entries with help of sheety app to the given google sheet.
+    In the beginning, personal properties and account informations should be given.
+    """
     def __init__(self) -> None:
         self.gender:str = ""
         self.height:int = 0
         self.weight:int = 0
         self.age:int = 0
         
-        self.headers:str= {"Authorization": "", 
-                           "x-app-id": "", 
-                           "x-app-key": "", 
+        self.headers:str= {"Authorization": AUTH_KEY_BEARER, 
+                           "x-app-id": APP_ID, 
+                           "x-app-key": APP_KEY, 
                            "content-type": "application/json"}
-        self.exercise_endpoint:str = "https://trackapi.nutritionix.com/v2/natural/exercise"
-        self.sheet_endpoint:str = "https://api.sheety.co/04fb3676394a9afdc57d00b4868f30df/workouts/tabellenblatt1"
+        self.exercise_endpoint:str = EXERCISE_ENDPOINT
+        self.sheet_endpoint:str = SHEET_ENDPOINT
         self.sheet_name:str = ""
-        
-        self.set_personal_properties()
-        self.set_account_infos()
         
     def add_exercise(self) -> None:
         """Checks first if endpoints and sheet name are valid. Then asks user the exercise(s). For given exercise(s)
         create row(s) in given sheet.
         """
-        date_today = dt.datetime.now().strftime("%d/%m/%Y")
-        time_now = dt.datetime.now().strftime("%X")
-        
         self.check_endpoints()
         self.check_sheet_name(sheet_name=self.sheet_name, input_message="Please enter the sheet name: ")
         exercise = input("Which exercise(s) did you do?:")
-        json_data = self.get_exercise_properties(exercise=exercise)
-
-        for exercise in json_data["exercises"]:
+        exercise_list = self.get_exercise_properties(exercise=exercise)
+        
+        date_today = dt.datetime.now().strftime("%d/%m/%Y")
+        time_now = dt.datetime.now().strftime("%X")
+        for exercise in exercise_list:
             sheet_inputs = {
                 self.sheet_name : {
                     "date": date_today,
@@ -87,6 +89,7 @@ class WorkoutTracker():
     
     def check_auth_token(self, auth_token:str) -> TrackerException or str:
         """Checks the given auth token. If its length <= 0, raises a TrackerException.
+        This auth token belongs to the specific project from Sheety website.
 
         Args:
             auth_token (str): Auth Token of sheety account.
@@ -105,12 +108,12 @@ class WorkoutTracker():
     def check_endpoints(self):
         """Checks the URL of the exercise and sheet endpoints. If there is a empty one, asks user for the URL.
         """
-        exercise_message = "Please give the URL of the exercise endpoint from nutritionix website."
-        sheet_endpoint_message = "Please give the URL of the sheet endpoint from sheety."
-        self.exercise_endpoint = self.check_url(url=self.exercise_endpoint, input_message=exercise_message)
+        exercise_endpoint_message = "Please give the URL of the exercise endpoint from nutritionix website: "
+        sheet_endpoint_message = "Please give the URL of the sheet endpoint from sheety: "
         self.sheet_endpoint = self.check_url(url=self.sheet_endpoint, input_message=sheet_endpoint_message)
+        self.exercise_endpoint = self.check_url(url=self.exercise_endpoint, input_message=exercise_endpoint_message)
         
-    def check_url(self, url:str, input_message:str) -> Exception or str:
+    def check_url(self, url:str, input_message:str = "Given URL is empty, please give a valid URL: ") -> Exception or str:
         """Checks the given URL if it is empty string or not.
         If it's a empty one, asks user for its URL and then validates it.
         If response is 200, returns the url. If not 200, raises an exception.
@@ -128,7 +131,7 @@ class WorkoutTracker():
         """
         if url == "":
             url = input(input_message)
-        if validators.url(url) == False:
+        if not validators.url(url):
             raise TrackerException(message=f"{url} is not valid.")
         return url
     
@@ -143,10 +146,31 @@ class WorkoutTracker():
         Returns:
             str: Sheet name in string data type.
         """
+        sheet_name_from_sheet_endpoint = self.sheet_endpoint.split("/")[-1]
         if sheet_name == "":
             sheet_name = input(input_message)
+        if sheet_name != sheet_name_from_sheet_endpoint:
+            raise TrackerException(message=f"Given sheet name {sheet_name} doesn't match with sheet name of sheet endpoint url {sheet_name_from_sheet_endpoint}.")
         return sheet_name
             
+            
+    def check_exercise_data(self, exercise_list:str) -> Exception or str:
+        """Checks the given exercise list. If its length is 0, raise an error to give warning to user.
+
+        Args:
+            exercise_list (str): Contains the exercises and their relevant informations.
+
+        Raises:
+            TrackerException: Exception, for to show the reason to user.
+
+        Returns:
+            Exception or str: If length is not 0, exercise list will be returned. Otherwise, an exception will occur.
+        """
+        if len(exercise_list) == 0:
+            raise TrackerException(message="There is no matching exercise data. Please try to write your exercise entry again.")
+        return exercise_list
+        
+        
     def check_weight(self, weight:int) -> int or Exception:
         """Checks the weight, if it's in a logical range it will be accepted and will be returned.
         If not in logical range, an exception will be raised.
@@ -210,79 +234,104 @@ class WorkoutTracker():
             "height_cm": self.height
         }
         response = requests.post(url=self.exercise_endpoint, json=parameters, headers=self.headers)
-        json_data = response.json()
-        print(response, json_data, response.text)
-        return json_data
+        exercise_list = self.check_exercise_data(exercise_data=response.json()["exercises"])
+        return exercise_list
         
         
     def get_rows(self) -> None:
-        """Prints the rows of the target google sheet.
+        """Prints the rows of the saved google sheet.
         If sheet name is not pre-defined, asks user for sheet name.
         """
         self.check_endpoints()
         response = requests.get(url=self.sheet_endpoint, headers=self.headers)
-        if self.sheet_name == "":
-            self.sheet_name = input("Please enter the sheet name from sheety:")
-        if self.sheet_name not in response.json():
-            raise TrackerException(message="Please check and consider changing the sheeet name again.")
+        self.sheet_name = self.check_sheet_name(sheet_name=self.sheet_name, input_message="Please enter the sheet name from sheety: ")
         rows = response.json()[self.sheet_name]
         for row in rows:
             print(f"Date: {row['date']}, exercise: {row['exercise']}, calorie: {row['calories']}")
     
     # SETTERS
-    def set_personal_properties(self) -> None:
-        """Asks user to his/hers physical properties for accurate evaluation.
-        Inputs should be in a logical range. If they're not, an exception will be raised.
-        """
-        self.set_gender()
-        self.set_age()
-        self.set_height()
-        self.set_weight()
-            
-            
-    def set_account_infos(self) -> None:
-        """Asks user to the APP ID, APP Key. If there is no, they will be stay as empty string.
-        Lastly it asks the auth token, if there is an auth method and sets these given variables
-        as self variables.
-        """
-        self.headers["x-app-id"] = self.check_app_id(input("Please enter your Nutritionix APP ID: "))
-        self.headers["x-app-key"] = self.check_app_key(input("Please enter your Nutritionix APP Key: "))
-        if(input("Is there a auth method? 'y' or 'n': ") == 'y'):
-            self.headers["Authorization"] = self.check_auth_token(input("Please enter the token(in format -> Basic/Bearer TOKEN): "))
-            
-            
-    def set_sheet_url(self, url:str) -> None:
-        """Assigns the given url as new endpoint of sheet.
+    def set_personal_properties(self, gender:str, age:int, height:int, weight:int) -> None:
+        """Takes 4 args. Gives these arguments to the functions, which they're belong.
 
         Args:
-            url (str): Url of the sheet from sheety app.
+            gender (str): Gender of the user, to be assigned as new gender of the account. 2 options, 'male' or 'female'. Not case sensitive.
+            age (int): Age of the user, to be assigned as new age of the account.
+            height (int): Height of the user, to be assigned as new height of the account.
+            weight (int): Weight of the user, to be assigned as new weight of the account.
         """
-        self.sheet_endpoint = url
+        self.set_gender(gender=gender)
+        self.set_age(age=age)
+        self.set_height(height=height)
+        self.set_weight(weight=weight)
+            
+    
+    def set_sheet_infos(self, sheet_name:str, authorization:str="") -> None:
+        """Sets the sheet infos for the assigned sheet url. If sheet url has an authorization method, authorization information
+        should be given too.
+
+        Args:
+            sheet_name (str): Sheet name of the project from the Sheety website.
+            authorization (str, optional): Authorization token for the sheet(Basic or Bearer). Defaults to "".
+        """
+        if authorization != "":
+            self.headers["Authorization"] = authorization
+        self.set_sheet_name(sheet_name=sheet_name)
+        
+        
+    def set_account_infos(self, app_id:str, app_key:str) -> None:
+        """Takes 3 args and gives these arguments to the checkers functions to be checked if they're valid.
+
+        Args:
+            app_id (str): APP Id of the user from Sheety website.
+            app_key (str): APP Key of the user from Sheety website.
+            authorization (str, optional): Auth token of the user from Sheety website. Defaults to "".
+        """
+        self.headers["x-app-id"] = self.check_app_id(app_id=app_id)
+        self.headers["x-app-key"] = self.check_app_key(app_key=app_key)
+            
+            
+    def set_exercise_url(self, exercise_url:str) -> None:
+        """Checks the given exercise url if it's valid or not and then assigns 
+        the given url as new endpoint of exercises, if the URL is valid.
+
+        Args:
+            url (str): Url of the exercises from nutritionix app.
+        """
+        self.exercise_endpoint = self.check_url(url=exercise_url)
+        
+    def set_sheet_url(self, sheet_url:str) -> None:
+        """Checks the given sheet endpoint url if it's valid or not and then assigns 
+        the given url as new endpoint of sheet, if the URL is valid.
+
+        Args:
+            url (str): Url of the sheet from Sheety website.
+        """
+        self.sheet_endpoint = self.check_url(url=sheet_url)
     
     def set_sheet_name(self, sheet_name:str) -> None:
-        """Changes the currently available sheet name with the given one.
+        """Checks the given sheet name if it's valid. Then changes the currently available sheet name with the given one.
 
         Args:
             sheet_name (str): String that contains the new sheet name, that will be changed with the old sheet name.
         """
-        self.sheet_name = sheet_name
+        self.sheet_name = self.check_sheet_name(sheet_name=sheet_name)
     
-    def set_weight(self) -> None:
-        """First checks the weight then assigns it as new weight.
+    def set_weight(self, weight:int) -> None:
+        """First checks the given weight, if it's in logical range, given weight will be assigned as new weight.
         """
-        self.weight = self.check_weight(int(input("Please enter your weight in kg: ")))
+        self.weight = self.check_weight(weight=weight)
     
-    def set_gender(self) -> None:
-        """First checks the gender then assigns it as new gender.
+    def set_gender(self, gender:str) -> None:
+        """First checks the given gender, if it's in logical range, given height will be assigned as new height.
         """
-        self.gender = self.check_gender(input("Please enter your gender: ").lower())
+        self.gender = self.check_gender(gender=gender)
     
-    def set_height(self) -> None:
-        """First checks the height then assigns it as new height.
+    def set_height(self, height:int) -> None:
+        """First checks the given height, if it's in logical range, given height will be assigned as new height.
         """
-        self.height = self.check_height(int(input("Please enter your height in cm: ")))
+        self.height = self.check_height(height=height)
     
-    def set_age(self) -> None:
-        """First checks the age then assigns it as new age.
+    def set_age(self, age:int) -> None:
+        """First checks the given age, if it's in logical range, given age will be assigned as new age.
         """
-        self.age = self.check_age(int(input("Please enter your age: ")))
+        self.age = self.check_age(age=age)
